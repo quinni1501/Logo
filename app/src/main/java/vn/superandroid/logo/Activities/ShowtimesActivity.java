eb68886d42338c3a89af694e372d7bca94495923
ngnfuong <nguyenngocdongphuong312@gmail.com>
2024-05-19 22:57:28 +0700

    showtimes

--- app/src/main/java/vn/superandroid/logo/Activities/ShowtimesActivity.java
+++ app/src/main/java/vn/superandroid/logo/Activities/ShowtimesActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,7 @@ import vn.superandroid.logo.R;
 public class ShowtimesActivity extends AppCompatActivity {
 
     Button gioBtn;
+    ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +21,18 @@ public class ShowtimesActivity extends AppCompatActivity {
 
         //ánh xạ
         gioBtn = findViewById(R.id.btn920);
+        backButton = findViewById(R.id.backButton);
         gioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ShowtimesActivity.this, SeatsActivity.class); // Thay thế ShowtimesActivity bằng SeatsActivity
                 startActivity(intent);
+            }
+        });
+       backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
