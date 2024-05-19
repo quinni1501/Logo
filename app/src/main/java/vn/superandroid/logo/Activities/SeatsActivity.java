4b1c7524aceac2adf2fe6cb3222cd0d7515b58e2
nhoclahola <125201610+nhoclahola@users.noreply.github.com>
2024-05-20 01:27:56 +0700

    Cấu hình để hiển thị chỗ ngồi cho Seats Activity

--- app/src/main/java/vn/superandroid/logo/Activities/SeatsActivity.java
+++ app/src/main/java/vn/superandroid/logo/Activities/SeatsActivity.java
@@ -3,13 +3,48 @@ package vn.superandroid.logo.Activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.superandroid.logo.Adapter.MoviesSeatAdapter;
 import vn.superandroid.logo.R;
 
 public class SeatsActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MoviesSeatAdapter adapter;
+    private List<String> seatList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_showtimes);
+        setContentView(R.layout.activity_seats);
+
+        recyclerView = findViewById(R.id.rvSeatsMoviesSeat);
+        // Grid Layout là để hiển thị theo dạng cột á, 1 hàng sẽ có 8 cột
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 8);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        // Giả lập danh sách các ghế
+        seatList = new ArrayList<>();
+        seatList.add("A1");
+        seatList.add("A2");
+        seatList.add("A3");
+        seatList.add("A4");
+        seatList.add("A5");
+        seatList.add("A6");
+        seatList.add("A7");
+        seatList.add("A8");
+        seatList.add("B1");
+        seatList.add("B2");
+        seatList.add("B3");
+        seatList.add("B4");
+        seatList.add("B5");
+
+        adapter = new MoviesSeatAdapter(seatList);
+        recyclerView.setAdapter(adapter);
     }
 }
