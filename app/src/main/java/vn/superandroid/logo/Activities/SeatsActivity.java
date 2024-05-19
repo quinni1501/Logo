c5425bd9a6e5643a973d4fb52d0582247da5fdc2
ngnfuong <nguyenngocdongphuong312@gmail.com>
2024-05-20 01:43:09 +0700

    SeatsActivity

--- app/src/main/java/vn/superandroid/logo/Activities/SeatsActivity.java
+++ app/src/main/java/vn/superandroid/logo/Activities/SeatsActivity.java
@@ -1,8 +1,11 @@
 package vn.superandroid.logo.Activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,14 +20,21 @@ public class SeatsActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MoviesSeatAdapter adapter;
     private List<String> seatList;
-
+    ImageButton backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seats);
 
+        // Set up the custom toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //ánh xạ
+        backBtn = findViewById(R.id.backButton);
         recyclerView = findViewById(R.id.rvSeatsMoviesSeat);
-        // Grid Layout là để hiển thị theo dạng cột á, 1 hàng sẽ có 8 cột
+        // Grid Layout là để hiển thị theo dạng cột, 1 hàng sẽ có 8 cột
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 8);
         recyclerView.setLayoutManager(gridLayoutManager);
 
@@ -43,8 +53,18 @@ public class SeatsActivity extends AppCompatActivity {
         seatList.add("B3");
         seatList.add("B4");
         seatList.add("B5");
+        seatList.add("B6");
+        seatList.add("B7");
+        seatList.add("B8");
 
         adapter = new MoviesSeatAdapter(seatList);
         recyclerView.setAdapter(adapter);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
